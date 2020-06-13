e92292b838250831f31548cc4b1ac561337ecc4e
codoc <radev2000@mail.ru>
2020-06-14 02:53:01 +0700

    фыав

--- acoder.py
+++ acoder.py
@@ -1,6 +1,7 @@
 import pickle
 
-FILE = "document1.txt" #название документа который надо закодировать
+FILE = "test.txt" #название документа который надо закодировать
+
 
 def open_file(): #открытие файла
     try:
@@ -10,71 +11,131 @@ def open_file(): #открытие файла
         return full
         file.close()
 
-def probability(text, SetListString):#вычисление вероятности встречи симбола в тексте
-    lenstr = len(text)
-    return [text.count(i) / lenstr for i in SetListString]
+def probability(text1):#вычисление вероятности встречи симбола в тексте
+    lene = len(text1)
+    text = text1
+    slovar = []
+    bykvi = []
+    count2=[]
+    count = []
+    while text:
+        bykvi.append(text[0])
+        count.append(text.count(text[0]))#цикл по нахождению символов
+        text = text.replace(text[0],"")
+    print(count, bykvi)
+
+
+
+    while count:
+        ind = count.index(max(count))
+        slovar.append(bykvi[ind])#распределение по числу
+        count2.append(count[ind])
+        count.pop(ind)
+        bykvi.pop(ind)
+
+    #print(count2)
+
+    otrezok = nahojdenie_otrezka(count2)
+    #print(otrezok)
+    code = codir(text1, otrezok, slovar)
+    #otpravka(lene, slovar, otrezok, code)
+    return count2
 
 def nahojdenie_otrezka(count):  #включаем наши симболы в один отрезок
-    otrezok = []
-    otrezok.append(count[0])
-    for i in range(1, len(count)):
-        otrezok.append(otrezok[i-1]+count[i])
-    otrezok[len(count)-1] = 1
+    otrezok = [0]
+    print(count)
+    for i in range(len(count)):
+        otrezok.append(otrezok[i]+count[i])
+
     return otrezok
+def BitsPlusFollow(bit):
+    global viv
+    global bits_to_follow
+    viv += str(bit)
+    while bits_to_follow > 0:
+        bits_to_follow-=1
+        if bit:
+            viv+='0'
+        else:
+            viv+='1'
 
 def codir(string, otrezok, bykvi):
-    ind = bykvi.index(string[0])#####кодируем выбранный отрезок
+    left = 0
+    global viv
+    global bits_to_follow
+    viv = ''
+    bits_to_follow = 0
+    str= ''
+    delitel = otrezok[-1]
+    print(bykvi)
 
-    granici = [0, 0] #границы закодированного отрезка
-    if ind == 0:
-        granici[0] = 0
+    right = 65535
+    First_qtr = (right+1)//4
+    Half = First_qtr*2
+    Third_qtr = First_qtr*3
+
+
+    for i in range(len(string)):# расчет нового отрезка
+        c = string[i]
+
+        ind = bykvi.index(c)
+        leftold = left
+        rightold = right
+        #
+        left = leftold + otrezok[ind]*(rightold - leftold + 1)//delitel
+        right = leftold + otrezok[ind+1]*(rightold - leftold + 1)//delitel - 1
+        print(left, right)
+        while True:
+            if right<Half:
+                BitsPlusFollow(0)
+                f = 1
+            elif left >= Half:
+
+                BitsPlusFollow(1)
+                left -= Half
+                right -= Half
+            elif (left >= First_qtr) and (right < Third_qtr):
+                bits_to_follow+=1
+                left -= First_qtr
+                right -= First_qtr
+            else:
+                break
+            left+=left
+            right+=right+1
+
+    bits_to_follow+=1
+    if left<First_qtr:
+        BitsPlusFollow(0)
     else:
-        granici[0] = otrezok[ind-1]
-    granici[1] = otrezok[ind]
+        BitsPlusFollow(1)
+    print(viv)
+    otpravka(bykvi, otrezok, viv)
+    return 1
 
-    for i in range(1, len(string)):# расчет нового отрезка
-        leftOLD = granici[0]
-        rightOLD = granici[1]
-
-        ind = bykvi.index(string[i])
-        if ind == 0:
-            RangeL = 0
-        else:
-            RangeL = otrezok[ind-1]
-        RangeR = otrezok[ind]
-        granici[0] = leftOLD+(rightOLD-leftOLD)*RangeL
-        granici[1] = leftOLD+(rightOLD-leftOLD)*RangeR
-
-    return granici[0]
-
-def otpravka(len, bykvi, otrezok, code):# отправка в файл
+def otpravka(bykvi, otrezok, code):# отправка в файл
+    lene = len(code)
+    n = int(code, 2)
+    n = n.to_bytes((n.bit_length() + 7) // 8, 'big')
+    print(n)
     with open('FILENAME.bin', "wb") as file:
-        pickle.dump(len, file)# сколько символов было
+        pickle.dump(lene, file)# сколько символов было
         pickle.dump(bykvi, file)# какие символы были
         pickle.dump(otrezok, file) #сколько их было
-        pickle.dump(code, file) #сами коды
+        pickle.dump(n, file) #сами коды
 
 
 def main():
     string = open_file() #открываем файл
     lene = len(string) #сколько символов всего
-    SetListString = set(list(string)) #какие символы в тексте
-    #print(list(string))
-    vozmojn = probability(string, SetListString) #вычисление вероятности встречи симбола в тексте
-    bykvi = [i for i in SetListString]
-    otrezok = nahojdenie_otrezka(vozmojn)
-    code = []
-    j = 0 #так как в конце каждой раскодируемой строки вызодит всякая фигня
-    i = 0 #я перекодирую последние 2 симбола
-    str = string[:13]
-    while str:
-        j+=2
-        i+=1
-        code.append(codir(str, otrezok, bykvi)) #добавление кода в семейство кодов
-        str = string[i*13-j:(i+1)*13-j]
 
-    otpravka(lene, bykvi, otrezok, code)
-    #print(vozmojn, set(list(string)))
+    #print(list(string))
+    vozmojn = probability(string) #вычисление вероятности встречи симбола в тексте
+    """bykvi = [i for i in SetListString]
+    otrezok = nahojdenie_otrezka(vozmojn)
+    code = (codir(string, otrezok, bykvi))
+
+    otpravka(lene, bykvi, otrezok, code)"""
+    #print(vozmojn, set(list(string)))"""
 
 
 main()
--- adecoder.py
+++ adecoder.py
@@ -10,30 +10,69 @@ def viv(text): #функция вывода
 
 def priem():
     with open(FILE, "rb") as file: #открытие файла
-        len = pickle.load(file)
+        lene = pickle.load(file)
         bykvi = pickle.load(file)
         otrezok = pickle.load(file)
         code = pickle.load(file)
+    m = int.from_bytes(code, byteorder ='big')
+    chislo = ''
+    print(bykvi, otrezok)
+    while m:
+        chislo = str(m%2) + chislo
+        m //= 2
+    #print(len(chislo))
+    a = (lene-len(chislo))
+    chislo = "0"*(a) + chislo
+    #print(chislo)
     ll = 0
     for i in otrezok: #я не знаю почему он сам не хочет искать количество, возможных значений
         ll+=1
     vivod = ''
     #print(string)
-    for h in code: #берем каждый код
-        for i in range(11): #в нем хорошо раскодируются 11 символов
-            for j in range(ll):
-                if h < otrezok[j]:
-                    if j == 0:
-                        RangeL = 0
-                    else:
-                        RangeL = otrezok[j-1]
-                    RangeR = otrezok[j]
-                    vivod += bykvi[j]
-                    break
-
-            h=(h-RangeL)/(RangeR-RangeL) #пересчет отрезка
-
-    return vivod[:len-1]
+    #print(chislo)
+    value = chislo[:16]
+    print(value)
+    value = int(value, 2)
+    print(value)
+    chislo = chislo[16:]
+    left = 0
+    right = 65535
+    delitel = otrezok[-1]
+    First_qtr = (right+1)//4
+    Half = First_qtr*2
+    Third_qtr = First_qtr*3
+    a = input()
+    while chislo:
+        leftold = left
+        rightold = right
+        freq = ((value - left +1)*delitel-1)//(right - left +1)
+        j = 1
+        while otrezok[j]<=freq:
+            j+=1
+        c = bykvi[j]
+        print(c)
+        left = leftold + otrezok[j]*(rightold - leftold + 1)//delitel
+        right = leftold + otrezok[j+1]*(rightold - leftold + 1)//delitel -1
+        while True:
+            if right <Half:
+                f = 1
+            elif left>=Half:
+                left -= Half
+                right -= Half
+                value -=Half
+            elif (left >= First_qtr) and (right < Third_qtr):
+                left -= First_qtr
+                right -= First_qtr
+                value -= First_qtr
+            else:
+                break
+            left*=2
+            right*=2 + 1
+            value+=value + int(chislo[0])
+            print(value, '\n')
+            chislo = chislo[1:]
+        vivod+=c
+    return vivod
 
 def main():
     text = priem()
